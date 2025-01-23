3b506a6851f3ea943a1e86c82453738257dcd40b
Thiago Campos <commit@thigcampos.com>
2025-01-23 10:16:01 -0300

    dotfiles automatically synced in 2025-01-23 10:16:01

--- cron/dot-cron/sync-dotfiles.sh
+++ cron/dot-cron/sync-dotfiles.sh
@@ -1,16 +1,19 @@
-#!/bin/bash
-
+#!/bin/zsh
 # Navigate to the dotfiles directory
 cd ~/.dotfiles
 
+# Use Homebrew's GPG path
+export PATH="/opt/homebrew/bin:$PATH"
+
 # Check for git changes
 if [[ -n $(git status --porcelain) ]]; then
-  # Get the current time
-  current_time=$(date +"%Y-%m-%d %H:%M:%S")
-  
-  # Add all changes to staging
-  git add .
+    # Get the current time
+    current_time=$(date +"%Y-%m-%d %H:%M:%S")
 
-  # Commit the changes with the message including the current time
-  git commit -m "dotfiles automatically synced in $current_time"
+    # Add all changes to staging
+    git add .
+
+    # Commit the changes with the message including the current time
+    git commit -m "dotfiles automatically synced in $current_time"
+    git push
 fi
